eadf56acdfc81989c06d329f7381afd83cd31df4
alumno <alumno@PC-XX>
2019-02-05 14:06:32 +0100

    primer commit

---
+++ proyecto_github/src/paquete_github/Clase_github.java
@@ -0,0 +1,11 @@
+package paquete_github;
+
+public class Clase_github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola mundo");
+	}
+
+}
